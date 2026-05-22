3406b8a46126df3f8f7c303455970d5987ad08ca
Eugenio <eugsilva12@gmail.com>
2026-05-22 20:00:15 -0300

    primeiros opcodes

--- chip8C.c
+++ chip8C.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 struct Chip_8{
     
     uint8_t registers[16];
     uint16_t I;
     uint16_t PC;
-    uint8_t SP;
+    uint8_t StackPointer;
     uint8_t DelayTimer;
     uint8_t SoundTimer;
     uint16_t stack[16];
@@ -27,3 +28,29 @@ int main() {
 
 
 }
+
+
+void fetch_and_decode(uint16_t opcode, struct Chip_8 *chip8){
+
+    switch ((opcode & 0xF00) >> 12)
+    {
+    case 0x0:
+        if((opcode & 0x000F) == 0x0){
+            memset(chip8->display, 0, sizeof(chip8->display));
+        }else{
+            chip8->StackPointer--;
+            chip8->PC = chip8->stack[chip8->StackPointer];
+        }
+        break;
+    
+    default:
+        break;
+    }
+
+
+
+
+}
+
+
+
